ac461a87ca701cfd9adca86ca18f62450d4b91e0
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-13 22:29:45 +0100

    Update webqr.js

--- webqr.js
+++ webqr.js
@@ -149,14 +149,14 @@ function htmlEntities(str) {
     return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
 
-// function read(a)
-// {
-//     var html="<br>";
-//     if(a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
-//         html+="<a target='_blank' href='"+a+"'>"+a+"</a><br>";
-//     html+="<b>"+htmlEntities(a)+"</b><br><br>";
-//     document.getElementById("result").innerHTML=html;
-// }	
+function read(a)
+{
+    var html="<br>";
+    if(a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
+        html+="<a target='_blank' href='"+a+"'>"+a+"</a><br>";
+    html+="<b>"+htmlEntities(a)+"</b><br><br>";
+    document.getElementById("result").innerHTML=html;
+}	
 
 function isCanvasSupported(){
   var elem = document.createElement('canvas');
