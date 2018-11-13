d4acbbf6071cebc0f82f874d33de89de57882291
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-13 22:06:39 +0100

    Create qr.php

---
+++ qr.php
@@ -0,0 +1,178 @@
+<!doctype html>
+<html lang="en"><head>
+    <title>How to decode QR code | PHPGang.com</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <style type="text/css">
+    
+      img {border-width: 0}
+      * {font-family:'Lucida Grande', sans-serif;}
+    </style>
+  </head>
+  <body  >
+  
+    
+    <div>
+      <h2>How to decode QR code example.&nbsp;&nbsp;&nbsp;=> <a href="http://www.phpgang.com/">Home</a> | <a href="http://demo.phpgang.com/">More Demos</a></h2>
+
+
+      
+        <div style="margin-top:8px">
+<iframe allowtransparency="true" frameborder="0" scrolling="no" src="http://platform.twitter.com/widgets/follow_button.1354093195.html#_=1354256562997&amp;id=twitter-widget-8&amp;lang=en&amp;screen_name=PHPGang&amp;show_count=true&amp;show_screen_name=true&amp;size=m" class="twitter-follow-button" style="width: 256px; height: 20px;" title="Twitter Follow Button" data-twttr-rendered="true"></iframe>
+</div>
+        <div style="float:left;width:90px">
+<a href="http://feeds2.feedburner.com/PHPGang" title="PHPGang feed"><img alt="feed" height="26" src="http://feeds2.feedburner.com/~fc/PHPGang?bg=f2a0bb&amp;fg=000000&amp;anim=0&amp;label=Readers" style="border:0;margin-right:10px;margin-top:6px" width="88"></a>
+</div>
+<div style="float:left;width:120px;margin-left:20px;margin-top:2px">
+<iframe allowtransparency="true" frameborder="0" scrolling="no" src="//www.facebook.com/plugins/subscribe.php?href=https%3A%2F%2Fwww.facebook.com%2Fhuzoorbux&amp;layout=button_count&amp;show_faces=true&amp;colorscheme=light&amp;font&amp;width=200&amp;appId=206841902768508" style="border:none; overflow:hidden; width:160px;height:25px;margin-top:5px"></iframe>
+</div>
+<!-- Place this tag where you want the badge to render. -->
+      
+      <div style="text-align: center;">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- PHPGang top demo all pages2 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:300px"
+     data-ad-client="ca-pub-6883622550208397"
+     data-ad-slot="8876576507"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+
+        <br /><br />
+      <hr />
+       
+  <p>
+               <style type="text/css">
+body{
+    width:100%;
+    text-align:center;
+}
+#qrfile{
+    width:320px;
+    height:240px;
+}
+#v{
+    width:320px;
+    height:240px;
+}
+#qr-canvas{
+    display:none;
+}
+#iembedflash{
+    margin:0;
+    padding:0;
+}
+#mp1{
+    text-align:center;
+    font-size:25px;
+}
+#mp2{
+    text-align:center;
+    font-size:25px;
+    color:red;
+}
+.selector{
+    border: solid;
+    border-width: 3px 3px 1px 3px;
+    margin:0;
+    padding:0;
+    cursor:pointer;
+    margin-bottom:-5px;
+}
+#outdiv
+{
+    width:320px;
+    height:240px;
+    border: solid;
+    border-width: 1px 1px 1px 1px;
+}
+#result{
+    border: solid;
+    border-width: 1px 1px 1px 1px;
+    padding: 0px;
+    width: 100%;
+}
+
+#imghelp{
+    position:relative;
+    left:0px;
+    top:-160px;
+    z-index:100;
+    font:18px arial,sans-serif;
+    background:#f0f0f0;
+    margin-left:35px;
+    margin-right:35px;
+    padding-top:10px;
+    padding-bottom:10px;
+    border-radius:20px;
+}
+p.helptext{
+    margin-top:54px;
+    font:18px arial,sans-serif;
+}
+p.helptext2{
+    margin-top:100px;
+    font:18px arial,sans-serif;
+}
+ul{
+    margin-bottom:0;
+    margin-right:40px;
+}
+li{
+    display:inline;
+    padding-right: 0.5em;
+    padding-left: 0.5em;
+    font-weight: bold;
+    border-right: 1px solid #333333;
+}
+li a{
+    text-decoration: none;
+    color: black;
+}
+
+#footer a{
+    color: black;
+}
+.tsel{
+    padding:0;
+}
+
+</style><script type="text/javascript" src="llqrcode.js"></script>
+
+<script type="text/javascript" src="webqr.js"></script>
+<script type="text/javascript" src="jquery-1.8.0.min.js"></script>
+<script>
+$(document).ready(function() {
+load();
+setimg();            
+});
+</script>
+<div id="main">
+<div id="mainbody">
+
+
+<div id="outdiv">
+
+</div>
+<div id="result"></div>
+</div></div>           <canvas id="qr-canvas" width="800" height="600"></canvas>        </p>            
+<div style='margin-left: auto;margin-right: auto;text-align: center;'>
+<br /><hr /><script id="mNCC" language="javascript">  medianet_width='300';  medianet_height= '600';  medianet_crid='973466122';  </script>  <script id="mNSC" src="http://contextual.media.net/nmedianet.js?cid=8CUV4W37I" language="javascript"></script> <br /><!-- START YieldSelect.com Private Marketplace Tag For PhpGang.com -->
+	    <!-- Size: 728x90 -->
+	    <script src="http://ads-by.yieldselect.com/tags/6417/246/async/728x90.js" type="text/javascript"></script>
+	    <!-- END YieldSelect.com Private Marketplace Tag For PhpGang.com --></div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-21769945-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
+  </body>
+</html>
