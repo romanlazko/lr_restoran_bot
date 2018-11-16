a053929bc3579ef3e3039cbe70efa9a81570ea92
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-16 18:35:18 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -1,15 +1,9 @@
 <?php
 function AddEntry() {
-    session_start();
-    require_once("dbconnect.php");
  
     echo "<BR>";
     echo "AddEntries Started";
     echo "<BR>";
-    $query = "INSERT INTO CRM.Clients (Name, Phone, Email)
-        VALUES ('John', '89161206482', 'john@example.com')";
-    $handler = mysql_query($query) or die ( "Error : ".mysql_error() );  
-    return $result;
 }
 AddEntry();
 ?>
