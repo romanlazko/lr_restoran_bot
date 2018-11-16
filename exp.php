c39285614d99686bf24728dc0f07ec7ec64c7f4d
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-16 18:34:25 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -1,12 +1,15 @@
-<SCRIPT LANGUAGE="JavaScript">
-        function clear() 
-        { 
-                return 'привет';
-        } 
-        var m=5;
-</SCRIPT>
- 
 <?php
-        echo "<p class='shout_text'>Сообщений нет!</p>";
-        echo "<script>alert(m);</script>"; //здесь ошибка отсутствует
+function AddEntry() {
+    session_start();
+    require_once("dbconnect.php");
+ 
+    echo "<BR>";
+    echo "AddEntries Started";
+    echo "<BR>";
+    $query = "INSERT INTO CRM.Clients (Name, Phone, Email)
+        VALUES ('John', '89161206482', 'john@example.com')";
+    $handler = mysql_query($query) or die ( "Error : ".mysql_error() );  
+    return $result;
+}
+AddEntry();
 ?>
