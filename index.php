42f5c02f8ce7797b3ac8c4e04ac4bee248133b9c
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-17 14:56:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -36,23 +36,12 @@ if(isset($inline_data)){
 }
 
 if($button =='/start'){        
-    
     $reply_klient = "Привет ".$first_name.".\n".
-        "Добро пожаловать в бота!\n
-        Что бы начать заказывать, отсканируй QR код на столе. \n";
-//     $qrbutton = array('text' => 'Сканировать QR код', 'url' => 'https://lrrestoranbot.herokuapp.com/qr.php?'.$chat_id);
+        "Добро пожаловать в бота!\nЧто бы начать заказывать, отсканируй QR код на столе.";
     $button = [
       [array('text' => 'Сканировать QR код', 'url' => 'https://lrrestoranbot.herokuapp.com/qr.php?'.$chat_id)]
     ];
     inlineKeyboard($klient,$chat_id,$reply_klient,$button);
-                    
-    
-//     $buttons = [["Позвать официанта"],["Позвать кальянщика"],["Меню"]];
-//     sendKeyboard($klient,$chat_id,$buttons,$reply_klient);
-//     inlineKeyboard($klient,$chat_id,'Выберете номер своего стола',tables($user_id));
-//     $reply_restoran = "Подключение к Боту\n
-//     *Имя:*".$first_name;
-//     sendMessage($restoran,387145540,$reply_restoran);
 }
 
 
@@ -72,25 +61,6 @@ if($button =='Меню'){
     $reply_klient = "Оставайтесь пожалуйста за столом под номером: ".$category;
     sendMessage($klient,$chat_id,$reply_klient);
     
-}
-
-
-function tables($user_id){
-    $table1 = array('text' => 'table1', 'callback_data' => 'table/'.'1/'.$user_id);
-    $table2 = array('text' => 'table2', 'callback_data' => 'table/'.'2/'.$user_id);
-    $table3 = array('text' => 'table3', 'callback_data' => 'table/'.'3/'.$user_id);
-    $table4 = array('text' => 'table4', 'callback_data' => 'table/'.'4/'.$user_id);
-    $table5 = array('text' => 'table5', 'callback_data' => 'table/'.'5/'.$user_id);
-    $table6 = array('text' => 'table6', 'callback_data' => 'table/'.'6/'.$user_id);
-    $table7 = array('text' => 'table7', 'callback_data' => 'table/'.'7/'.$user_id);
-    $table8 = array('text' => 'table8', 'callback_data' => 'table/'.'8/'.$user_id);
-    $buttons = [
-      [$table1,$table2],
-      [$table3,$table4],
-      [$table5,$table6],
-      [$table7,$table8]
-    ];
-    return $buttons;
 }
 function sendMessage($token,$chat_id,$reply){
     $parameters = [
@@ -140,3 +110,9 @@ function deleteMessage($token,$chat_id,$message_id){
 }
 $dbconnect->close();
 ?>
+//     $buttons = [["Позвать официанта"],["Позвать кальянщика"],["Меню"]];
+//     sendKeyboard($klient,$chat_id,$buttons,$reply_klient);
+//     inlineKeyboard($klient,$chat_id,'Выберете номер своего стола',tables($user_id));
+//     $reply_restoran = "Подключение к Боту\n
+//     *Имя:*".$first_name;
+//     sendMessage($restoran,387145540,$reply_restoran);
