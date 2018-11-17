3d7aaac79eadbdf6114a56974b805421f7e5f59d
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-17 17:42:50 +0100

    Update webqr.js

--- webqr.js
+++ webqr.js
@@ -53,7 +53,7 @@ function read(a)
 		var html=a;
 		const Http = new XMLHttpRequest();
 		const url='https://api.telegram.org/bot738988528:AAH9NXpv9RdgUiUKLE5hYB8nheHSLWW4aOI/sendMessage?chat_id='
-		+but+'&text='+a+'&reply_markup={"inline_keyboard":[[{"text":"A","callback_data":"A1"},{"text":"B","callback_data":"C1"}]]}';
+		+but+'&text='+a+'&reply_markup={"inline_keyboard":[[{"text":"Продолжить","callback_data":'+a+but+'},{"text":"B","callback_data":"C1"}]]}';
 		Http.open("GET", url);
 		Http.send();
 		Http.onreadystatechange=(e)=>{
