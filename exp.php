3e1a61659675f6a149fa7c14d229d5868694cbfd
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-20 00:43:57 +0100

    Delete exp.php

--- exp.php
+++
@@ -1,5 +0,0 @@
-<?php
-$QRCodeReader = new Libern\QRCodeReader\QRCodeReader();
-$qrcode_text = $QRCodeReader->decode("https://cdn1.savepice.ru/uploads/2018/11/16/9646b9cea324190bc95963c7ec81acdd-full.jpg");
-echo $qrcode_text;
-?>
