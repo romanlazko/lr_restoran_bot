e045cbf63ccc3409901f9086c37b5daa64a432a7
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-20 16:58:58 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -2,7 +2,7 @@
     $inline_data = 'кнопка/стол/имя/айди';
     $str = substr($inline_data, 0, strrpos($inline_data, '/'));
     $str2 = substr($str, 0, strrpos($str, '/'));
-    $button = substr($str2, 0, strrpos($str, '/'));
+    $button = substr($str2, 0, strrpos($str2, '/'));
     $table = substr($str, strrpos($str,"/")+1);    
     $pos_name = substr($inline_data, strrpos($inline_data,"/")+1);
 //     $pos_id = 
