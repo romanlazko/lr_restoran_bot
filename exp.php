c17c0c1542e846cb09b584b25b585e1264f857ff
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-20 17:00:19 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -3,7 +3,7 @@
     $str = substr($inline_data, 0, strrpos($inline_data, '/'));
     $str2 = substr($str, 0, strrpos($str, '/'));
     $button = substr($str2, 0, strrpos($str2, '/'));
-    $table = substr($str, strrpos($str,"/")+1);    
-    $pos_name = substr($inline_data, strrpos($inline_data,"/")+1);
+    $table = substr($str2, strrpos($str2,"/")+1);    
+    $pos_name = substr($str, strrpos($str,"/")+1);
 //     $pos_id = 
     echo $str.' '.$button.' '.$table.' '.$pos_name.' ';
