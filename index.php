1db937fd33bb1837c49dea4cf4a7af232956c6dc
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-24 15:14:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -100,17 +100,18 @@ if($button =='noconfirm'){
     editMassage($klient,$chat_id,$message_id,$reply_klient,order($table,1,$pos_id));
 }
 if($button =='confirm'){
-    $buttons =[
-        [array('text'=>"Ждите",'callback_data'=>"o/1/2/3")]
-    ];
-    
-    editMessageReplyMarkup($klient,$chat_id,$message_id,$buttons);
-    $reply_klient = posData($pos_id)['pos_name'];
-    editMassage($klient,$chat_id,$message_id,$reply_klient,order($table,1,$pos_id));
-//     if($message ==$reply_klient){
-        $reply_restoran = $message."Стол: ".$table."\nЗаказ: ".$pos_id."\nКоличество: ".$pos_name; 
-        inlineKeyboard($restoran,$chat_id,$reply_restoran,confirm($table,$pos_name,$pos_id));
-//     }        
+    answerCallbackQuery($klient, $output['callback_query']['id'], "проверка ", false);
+//     $buttons =[
+//         [array('text'=>"Ждите",'callback_data'=>"o/1/2/3")]
+//     ];
+//     $output = json_decode(file_get_contents('php://input'),true);
+//     editMessageReplyMarkup($klient,$chat_id,$message_id,$buttons);
+//     $reply_klient = posData($pos_id)['pos_name'];
+//     editMassage($klient,$chat_id,$message_id,$reply_klient,order($table,1,$pos_id));
+// //     if($message ==$reply_klient){
+//         $reply_restoran = $message."Стол: ".$table."\nЗаказ: ".$pos_id."\nКоличество: ".$pos_name; 
+//         inlineKeyboard($restoran,$chat_id,$reply_restoran,confirm($table,$pos_name,$pos_id));
+// //     }        
 }
 if($button =='accept'){
     $reply_restoran = $message;
@@ -190,6 +191,9 @@ function editMessageReplyMarkup($token,$chat_id,$message_id,$buttons){
     ];
     file_get_contents('https://api.telegram.org/bot' . $token . '/editMessageReplyMarkup?' . http_build_query($parameters).'&parse_mode=Markdown');
     return TRUE;
+}
+function answerCallbackQuery($token, $callback_query_id, $text, $show_alert){
+    file_get_contents("https://api.telegram.org/bot".$token."/answerCallbackQuery?callback_query_id=".$callback_query_id."&text=".$text."&show_alert=".$show_alert);
 }
 // function deleteMessage($token,$chat_id,$message_id){     
 //     $parameters = [
