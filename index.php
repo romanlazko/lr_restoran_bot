56cdfce3bb6c181c13b55a25ce3cc83e12843132
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-25 20:42:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -44,38 +44,21 @@ function orderfunc($klient,$restoran,$chat_id,$pos_id,$user_id,$order_time,$id,$
     $dbconnect = new mysqli($servername, $username, $password, $dbname);
     
     $result = $dbconnect->query("SELECT id FROM order_id");
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()){        
-            if($row['id'] != $id){
-                $orderInsert = "INSERT INTO order_id(user_id,id,order_text,order_time) VALUES('$user_id','$id','$pos_id','$order_time')";            
-                if($dbconnect->query($orderInsert) === TRUE){
-                    $reply_restoran = "Стол: ".$table."\nЗаказ: ".$pos_id."\nКоличество: ".$pos_name; 
-                    inlineKeyboard($restoran,$chat_id,$reply_restoran,confirm($table,$pos_name,$pos_id));
-                } 
-            }
-        }
-        
-    }
     
-//     else {
-//         $promocodeInsert = $dbconnect->query("INSERT INTO promocodes(pos_id,user_id,promocode) 
-//                                               VALUES('$pos_id','$user_id','$promocode')");
-//         return $promocode;
-//     }
-//     while($row = $result->fetch_assoc()){
+    while($row = $result->fetch_assoc()){
         
-//         if($row['id']==$id){
-//             $new_id = false;
-//             break;
-//         }
-//     }   
-//     if($new_id !== false){
-//         $orderInsert = "INSERT INTO order_id(user_id,id,order_text,order_time) VALUES('$user_id','$id','$pos_id','$order_time')";            
-//         if($dbconnect->query($orderInsert) === TRUE){
-//             $reply_restoran = "Стол: ".$table."\nЗаказ: ".$pos_id."\nКоличество: ".$pos_name; 
-//             inlineKeyboard($restoran,$chat_id,$reply_restoran,confirm($table,$pos_name,$pos_id));
-//         } 
-//     }
+        if($row['id']==$id){
+            $new_id = false;
+            break;
+        }
+    }   
+    if($new_id !== false){
+        $orderInsert = "INSERT INTO order_id(user_id,id,order_text,order_time) VALUES('$user_id','$id','$pos_id','$order_time')";            
+        if($dbconnect->query($orderInsert) === TRUE){
+            $reply_restoran = "Стол: ".$table."\nЗаказ: ".$pos_id."\nКоличество: ".$pos_name; 
+            inlineKeyboard($restoran,$chat_id,$reply_restoran,confirm($table,$pos_name,$pos_id));
+        } 
+    }
         
     $dbconnect->close();
 }
