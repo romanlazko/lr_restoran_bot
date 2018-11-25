92bc3bc6362b4424051237f99275f5fbff4a0333
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-11-25 19:45:29 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -1,12 +1,17 @@
 <?php
-    $inline_data = 'кнопка/стол/имя/айди';
-    $str = substr($inline_data, 0, strrpos($inline_data, '/'));
-    $str2 = substr($str, 0, strrpos($str, '/'));
-    $button = substr($str2, 0, strrpos($str2, '/'));
-    $table = substr($str2, strrpos($str2,"/")+1);    
-    $pos_name = substr($str, strrpos($str,"/")+1);
-    $str3 = substr($inline_data, strrpos($inline_data,"/")+1);
-    $pos_id = substr($str3, 0, strrpos($str3, '?'));
-    $order_id = substr($str3, strrpos($str3,"?")+1);
+    $inline_data = 'кнопка/стол/имя/айди/09876';
+    substr_count($inline_data,'/');
 
-    echo $button.' '.$table.' '.$pos_name.' '.$pos_id.' '.$order_id;
+    for ($i = 1; $i <= substr_count($inline_data,'/'); $i++) {
+        echo substr($inline_data, 0, strrpos($inline_data, '/');
+    }
+//     $str = substr($inline_data, 0, strrpos($inline_data, '/'));
+//     $str2 = substr($str, 0, strrpos($str, '/'));
+//     $button = substr($str2, 0, strrpos($str2, '/'));
+//     $table = substr($str2, strrpos($str2,"/")+1);    
+//     $pos_name = substr($str, strrpos($str,"/")+1);
+//     $str3 = substr($inline_data, strrpos($inline_data,"/")+1);
+//     $pos_id = substr($str3, 0, strrpos($str3, '?'));
+//     $order_id = substr($inline_data, strrpos($inline_data,"/")+1);
+
+    //echo $button.' '.$table.' '.$pos_name.' '.$pos_id.' '.$order_id;
