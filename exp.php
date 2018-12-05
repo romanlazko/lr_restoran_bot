accb068c7f09f0e3c49916bee86a787a7628c7ab
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-12-05 13:23:25 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -1,14 +1,17 @@
 <?php
-    $inline_data = 'кнопка/стол/имя/айди';
-    $str = substr($inline_data, 0, strrpos($inline_data, '/'));
-    $str2 = substr($str, 0, strrpos($str, '/'));
-    $str3 = substr($str2, 0, strrpos($str2, '/'));
-    $button = substr($str3, 0, strrpos($str3, '/'));
-    $table = substr($str3, strrpos($str3,"/")+1);    
-    $pos_name = substr($str2, strrpos($str2,"/")+1);
+//     $inline_data = 'кнопка/стол/имя/айди';
+//     $str = substr($inline_data, 0, strrpos($inline_data, '/'));
+//     $str2 = substr($str, 0, strrpos($str, '/'));
+//     $str3 = substr($str2, 0, strrpos($str2, '/'));
+//     $button = substr($str3, 0, strrpos($str3, '/'));
+//     $table = substr($str3, strrpos($str3,"/")+1);    
+//     $pos_name = substr($str2, strrpos($str2,"/")+1);
     
-    $pos_id = substr($str, strrpos($str,"/")+1);
-    $order_id = substr($inline_data, strrpos($inline_data,"/")+1);
+//     $pos_id = substr($str, strrpos($str,"/")+1);
+//     $order_id = substr($inline_data, strrpos($inline_data,"/")+1);
 
-    echo $button.' '.$table.' '.$pos_name.' '.$pos_id.' '.$order_id.' ';
+//     echo $button.' '.$table.' '.$pos_name.' '.$pos_id.' '.$order_id.' ';
+require __DIR__ . "/vendor/autoload.php";
+$qrcode = new QrReader('qrcode.jpg');
+$text = $qrcode->text();
 ?>
